5ddf0675e240aecba4af9b315d20f9e19704ddc3
Kristyyyy <kristy.pangilinan29@gmail.com>
2026-01-26 21:28:35 +0800

    Add files via upload

---
+++ finalclock/waveClock/SquareLine_Project_events.py
@@ -0,0 +1,16 @@
+
+def choseTime(event_struct):
+    return
+
+
+def prepareTime(event_struct):
+    return
+
+
+def cngBright(event_struct):
+    return
+
+
+def prepareEmotion(event_struct):
+    return
+
